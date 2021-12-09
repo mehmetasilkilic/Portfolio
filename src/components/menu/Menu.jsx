2258afd1dd546cce1d542de076e52e1de3a1d6f5
mehmetasilkilic <mehmetasil96@gmail.com>
2021-12-09 13:38:51 +0300

    c

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,15 +1,36 @@
 import "./menu.scss";
+import { motion } from "framer-motion";
 
 const Menu = ({ menuOpen, setMenuOpen }) => {
   return (
     <div className={"menu " + (menuOpen && "active")}>
       <ul>
-        <li onClick={() => setMenuOpen(false)}>
+        <motion.li
+          whileHover={{
+            originX: 0,
+            scale: 1.3,
+            textShadow: "0px 0px 5px rgb(255,255,255)",
+            transition: {
+              type: 'spring',
+              stiffness: 300,
+            }
+          }}
+          onClick={() => setMenuOpen(false)}>
           <a href="#intro">Home</a>
-        </li>
-        <li onClick={() => setMenuOpen(false)}>
-          <a href="#portfolio">Latest Works</a>
-        </li>
+        </motion.li>
+        <motion.li
+          whileHover={{
+            originX: 0,
+            scale: 1.3,
+            textShadow: "0px 0px 5px rgb(255,255,255)",
+            transition: {
+              type: 'spring',
+              stiffness: 300,
+            }
+          }}
+          onClick={() => setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </motion.li>
       </ul>
     </div>
   );
