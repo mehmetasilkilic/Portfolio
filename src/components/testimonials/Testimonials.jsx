11cd9662c6fac0bfd48c088ddaf40dc75c7a6e75
mehmetasilkilic <mehmetasil96@gmail.com>
2021-12-10 08:23:40 +0300

    c

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -11,7 +11,7 @@ const Testimonials = () => {
         "https://www.crystalcare.org.uk/wp-content/uploads/2014/05/pp-blank-m.jpg",
       icon: "assets/twitter.png",
       desc:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam doloresm.",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placet magnam doloresm.",
     },
     {
       id: 2,
